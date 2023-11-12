6b4c8824400fa23b90a30dfb3bb67d359b3e6bcd
gulbi <123242543+gulbiarchive@users.noreply.github.com>
2023-11-13 02:05:19 +0900

    Add files via upload

---
+++ Simple-Project/project6.py
@@ -0,0 +1,40 @@
+n1 = input() # 학생 번호
+n2 = input()
+n3 = input()
+n4 = input()
+n5 = input()
+n6 = input()
+n7 = input()
+
+a1 = input() # 학생 번호
+a2 = input()
+a3 = input()
+a4 = input()
+a5 = input()
+a6 = input()
+a7 = input()
+
+print('안녕하세요! 저는 이번에 새로 임용된 중학교 교사 철수입니다.')
+print()
+
+print('출근 \'첫날\'이라 일찍 왔어요.')
+print('살짝 긴장되지만 그래도 열심히 해야겠죠?')
+print()
+
+print('너무 일찍 왔는지 학교에 아무도 없어요.')
+print('학생들 번호를 제가 정해야겠어요!')
+print()
+# 더하기 연산자 : 같은 자료형끼리 가능
+print('>> 학생 번호와 이름 <<')
+print(n1 + '번 학생의 이름은 ' + a1)
+print(n2 + '번 학생의 이름은 ' + a2)
+print(n3 + '번 학생의 이름은 ' + a3)
+print(n4 + '번 학생의 이름은 ' + a4)
+print(n5 + '번 학생의 이름은 ' + a5)
+print(n6 + '번 학생의 이름은 ' + a6)
+print(n7 + '번 학생의 이름은 ' + a7)
+print()
+
+print('마지막 학생의 이름이 기억나지 않아요 ㅠㅠ')
+print('학교에 오면 물어봐야겠어요..')
+
---
+++ Simple-Project/project8.py
@@ -0,0 +1,36 @@
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+n5 = int(input())
+n6 = int(input())
+n7 = int(input())
+n8 = int(input())
+n9 = int(input())
+n10 = int(input())
+n11 = int(input())
+n12 = int(input())
+
+print('우린 이제 더하기, 빼기 연산자를 알고 있어요.')
+print('배우진 않았지만 익숙하죠?')
+print()
+
+print('저도 따로 배우진 않았는데 \'익숙\'했어요!!')
+print()
+
+print('"더하기" 먼저 할까요? "빼기" 먼저 할까요?')
+print('음.. @@더하기@@ 부터 할게요!')
+print()
+
+print('>> 더하기 부분 <<')
+print(n1, '+', n2, '=', n1 + n2)
+print(n3, '+', n4, '=', n3 + n4)
+print(n5, '+', n6, '=', n5 + n6)
+print()
+
+print('>> 빼기 부분 <<')
+print(n7, '-', n8, '=', n7 - n8)
+print(n9, '-', n10, '=', n9 - n10)
+print(n11, '-', n12, '=', n11 - n12)
+
+
